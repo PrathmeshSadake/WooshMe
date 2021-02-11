5ccfc3d134c56c39e6d000bb9f2e7db74e1bd535
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-11 23:22:42 +0530

    Displaying messages from firestore using stream builder

---
+++ lib/widgets/chat/messages.dart
@@ -0,0 +1,35 @@
+import 'package:WooshMe/widgets/chat/message_bubble.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapshot) {
+        if (chatSnapshot.connectionState == ConnectionState.waiting) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        final chatDocs = chatSnapshot.data.docs;
+        User user = FirebaseAuth.instance.currentUser;
+        return ListView.builder(
+          reverse: true,
+          itemCount: chatDocs.length,
+          itemBuilder: (ctx, index) => MessageBubble(
+            chatDocs[index]['text'],
+            chatDocs[index]['username'],
+            chatDocs[index]['userId'] == user.uid,
+            // key: ValueKey(chatDocs[index].documentID),
+          ),
+        );
+      },
+    );
+  }
+}
