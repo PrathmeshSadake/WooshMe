ac47e5890d0e299cc66db66738190559a2304ea7
Prathmesh Sadake <prathmeshsadake@gmail.com>
2021-02-11 23:15:49 +0530

    Moved auth form to separate file

---
+++ lib/widgets/auth/auth_form.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class AuthForm extends StatefulWidget {
+  final bool isLoading;
+  final void Function(
+    String email,
+    String username,
+    String password,
+    bool isLogin,
+    BuildContext ctx,
+  ) submitFn;
+  AuthForm(this.submitFn, this.isLoading);
+
+  @override
+  _AuthFormState createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _isLogin = true;
+  var _userEmail = '';
+  var _userName = '';
+  var _userPassword = '';
+
+  void _trySubmit() {
+    final _isValid = _formKey.currentState.validate();
+    FocusScope.of(context).unfocus(); //close the keyboard
+    if (_isValid) {
+      _formKey.currentState.save();
+      widget.submitFn(
+        // trim() is used to remove all the white spaces from the end
+        _userEmail.trim(),
+        _userName.trim(),
+        _userPassword.trim(),
+        _isLogin,
+        context,
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        margin: EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(
+              18,
+            ),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextFormField(
+                    key: ValueKey('email'),
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@')) {
+                        return 'Please enter a valid email address';
+                      } else {
+                        return null;
+                      }
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email address',
+                    ),
+                    onChanged: (value) {
+                      _userEmail = value;
+                    },
+                  ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      validator: (value) {
+                        if (value.isEmpty || value.length < 4) {
+                          return 'Username must contain atleast 4 characters';
+                        } else {
+                          return null;
+                        }
+                      },
+                      decoration: InputDecoration(labelText: 'Username'),
+                      onChanged: (value) {
+                        _userName = value;
+                      },
+                    ),
+                  TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7) {
+                        return 'Password must be atleast 8 characters long';
+                      } else {
+                        return null;
+                      }
+                    },
+                    decoration: InputDecoration(labelText: 'Password'),
+                    obscureText: true,
+                    onChanged: (value) {
+                      _userPassword = value;
+                    },
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  if (widget.isLoading) CircularProgressIndicator(),
+                  if (!widget.isLoading)
+                    RaisedButton(
+                      child: Text(_isLogin ? 'Login' : 'Signup'),
+                      onPressed: _trySubmit,
+                    ),
+                  if (!widget.isLoading)
+                    FlatButton(
+                      child: Text(
+                        _isLogin
+                            ? 'Create a new account'
+                            : 'I already have an account',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      },
+                    )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
